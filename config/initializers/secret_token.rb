287c4f0ba38969df1852c1119306b7f6113aa342
Helen Hood <helen.c.hood@gmail.com>
2014-03-18 14:52:25 -0400

    Inital commit - new Rails app with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BirdTracker::Application.config.secret_key_base = 'e1eeff51a3eb468d3dddc753d530c6665131030fcd357ba0eb0eb739e342dba9760e359235ee761ad5103241df586293d4ece0bd52a77323feab95005feb2883'
